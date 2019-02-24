0773bc554f42b5a4380e3df0b915deb8186321ee
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-24 17:05:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
   
 end
 
-def add_element_to_start_of_array(array, Pastor Grace)
+def add_element_to_start_of_array(array, element)
   array.unshift("Pastor Grace")
   
 end
