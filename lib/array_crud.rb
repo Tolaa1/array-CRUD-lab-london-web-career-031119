bd0b8af73fb5cc04bd1d802791dcb36e9d375c3f
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-24 17:22:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push
+  array.push(element)
   
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift
+  array.unshift(element)
   
 end
 
