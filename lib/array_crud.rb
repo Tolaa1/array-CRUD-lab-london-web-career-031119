e6e3e3f076f805d51a3a7a140b1fa73387245908
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-24 16:57:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,37 +4,37 @@ def create_an_empty_array
 end
 
 def create_an_array
- Loyalty = ["Tola","Ade","Araoluwa","cynthia"]
+ array = ["Tola","Ade","Araoluwa","cynthia"]
 end
 
-def add_element_to_end_of_array(Loyalty, "Yem")
-  Loyalty.push("Yem")
+def add_element_to_end_of_array(array, "Yem")
+  array.push("Yem")
   
 end
 
-def add_element_to_start_of_array(Loyalty, "Pastor Grace")
-  Loyalty.unshift("Pastor Grace")
+def add_element_to_start_of_array(array, "Pastor Grace")
+  array.unshift("Pastor Grace")
   
 end
 
-def remove_element_from_end_of_array(Loyalty)
-  Loyalty.pop("Ade")
+def remove_element_from_end_of_array(array)
+  array.pop("Ade")
   
 end
 
-def remove_element_from_start_of_array(Loyalty)
-  Loyalty.shift("Yem")
+def remove_element_from_start_of_array(array)
+  array.shift("Yem")
   
 end
 
-def retrieve_element_from_index(Loyalty, 1)
-  Loyalty.index[1]
+def retrieve_element_from_index(array, 1)
+  array.index[1]
 end
 
-def retrieve_first_element_from_array(Loyalty)
-  Loyalty.index[0]
+def retrieve_first_element_from_array(array)
+  array.index[0]
 end
 
-def retrieve_last_element_from_array(Loyalty)
-  Loyalty.index[-1]
+def retrieve_last_element_from_array(array)
+  array.index[-1]
 end
